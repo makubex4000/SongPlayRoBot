1624afb3540ec8df40990989f35e207803c3e7df
KevinChen <67411136+makubex2010@users.noreply.github.com>
2021-12-16 09:13:28 +0800

    Update tgmusicbot.py

--- tgmusicbot.py
+++ tgmusicbot.py
@@ -47,7 +47,7 @@ from datetime import timedelta
 from urllib.parse import urlparse
 from pyrogram import Client, filters, idle
 from pyrogram.types import Message
-from youtube_dl import YoutubeDL
+from yt-dlp import YoutubeDL
 from PIL import Image
 import ffmpeg
 
